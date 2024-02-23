cd1f4bf75d0f3285d5d2f35c17259c65716890e3
mayanktiwari58 <mt3260273@gmail.com>
2024-02-23 12:12:08 +0530

    feat:(Search Input Box)

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,8 +1,15 @@
-import { ActivityIndicator, StyleSheet, Text, View,FlatList } from 'react-native'
-import React from 'react'
-import { gql, useQuery } from '@apollo/client';
-import BookItem from '../components/BookItem';
-
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
+import { gql, useQuery,useLazyQuery } from "@apollo/client";
+import BookItem from "../components/BookItem";
 
 const query = gql`
   query SearchBooks($q: String) {
@@ -37,50 +44,68 @@ const query = gql`
 `;
 
 const TabOneScreen = () => {
-    const { data, loading, error } = useQuery(query, {
-        variables: { q: 'React Native'},
-      });
+  const [search, setSearch] = useState("");
+  const [runQuery, { data, loading, error }] = useLazyQuery(query);
+  
 
-      console.log(data);
-      console.log(loading);
-      console.log(error);
   return (
     <View style={styles.container}>
-        {loading && <ActivityIndicator/>}
-        {error && (
+      <View style={styles.header}>
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="search..."
+          style={styles.input}
+        />
+        <Button title="search" onPress={()=>runQuery({variables:{q:search}})} />
+      </View>
+      {loading && <ActivityIndicator />}
+      {error && (
         <View style={styles.container}>
           <Text style={styles.title}>Error fetching books</Text>
           <Text>{error.message}</Text>
         </View>
       )}
-     <FlatList
-  data={data?.googleBooksSearch?.items || []}
-  renderItem={({ item }) => (
-    <BookItem
-      book={{
-        title: item.volumeInfo.title,
-        image: item.volumeInfo.imageLinks.thumbnail,
-        authors: item.volumeInfo.authors,
-        isbn: "",
-      }}
-    />
-  )}
-  showsVerticalScrollIndicator={false}
-/>
+      <FlatList
+        data={data?.googleBooksSearch?.items || []}
+        renderItem={({ item }) => (
+          <BookItem
+            book={{
+              title: item.volumeInfo.title,
+              image: item.volumeInfo.imageLinks?.thumbnail,
+              authors: item.volumeInfo.authors,
+              isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
+            }}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:10,
-       
-    },
-    title:{
-        fontSize:25,
-    }
-})
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 25,
+  },
+  header: {
+    flexDirection: "row",
+    padding: 1,
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 7,
+    padding: 5,
+    marginVertical: 5,
+    borderColor: "#808080",
+  },
+});
 
 export default TabOneScreen;
-
